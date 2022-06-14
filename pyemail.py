add7b1dce4ce665b39c278f81fbcd8653278770d
Brycezsq <56464946+Brycezsq@users.noreply.github.com>
2022-06-14 11:06:05 +0800

    Update pyemail.py

--- pyemail.py
+++ pyemail.py
@@ -3,14 +3,14 @@ from email.mime.text import MIMEText
 from email.header import Header
 
 mail_host = "smtp.qq.com"  # 设置服务器
-mail_user = "2433929899@qq.com"  # 用户名
-mail_pass = "cbhwtehbdhuqebbh"  # 授权码，注意不是邮箱登录密码，是上述设置的授权密码！！！
-sender = '2433929899@qq.com'
-receivers = ['2433929899@qq.com', '760506306@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+mail_user = ""  # 用户名
+mail_pass = ""  # 授权码，注意不是邮箱登录密码，是上述设置的授权密码！！！
+sender = ''
+receivers = ['']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
 
 message = MIMEText('test', 'plain', 'utf-8')
-message['From'] = "2433929899@qq.com"
-message['To'] = "2433929899@qq.com,760506306@qq.com"
+message['From'] = ""
+message['To'] = ""
 subject = 'test email'
 message['Subject'] = Header(subject, 'utf-8')
 smtpObj = smtplib.SMTP()
